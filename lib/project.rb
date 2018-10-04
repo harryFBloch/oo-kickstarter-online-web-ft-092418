74c71bf5f369a329b9cf9e1ddbef0383d6190768
Harry Bloch <harryfbloch@gmail.com>
2018-10-04 01:19:51 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,12 +1,13 @@
 class Project
-  attr_accessor :title
+  attr_accessor :title, :backers
   
   def initialize(title)
     self.title = title
+    self.backers = []
   end
   
   def add_backers(name)
-    
+    self.backers << Backer.new(name)
   end
   
 end
